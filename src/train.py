41d87c1eecbfcb8caf26f8dd8717f694f55de8ee
unknown <kprabhu@ebay.com>
2016-10-21 23:13:18 -0700

    Change model to LSTM

--- src/train.py
+++ src/train.py
@@ -46,10 +46,18 @@ from keras.optimizers import SGD, RMSprop, Adadelta, Adagrad, Adam, Adamax
 from keras.regularizers import l2
 
 net = Sequential()
-net.add(Dense(320, input_dim=xTrain.shape[1], init='glorot_uniform', activation='tanh'))
-net.add(Dropout(0.5))
-net.add(Dense(160, init='glorot_uniform', activation='tanh'))
-net.add(Dense(1, init='uniform', activation='linear'))
+net.add(LSTM(128, input_shape=(347, 1), return_sequences=False, go_backwards=False,
+               W_regularizer=l2(0.005), U_regularizer=l2(0.005),
+               inner_init='glorot_normal', init='glorot_normal', activation='tanh'))  # try using a GRU instead, for fun
+net.add(Dense(1, activation='linear'))
+
+# try using different optimizers and different optimizer configs
+net.compile(loss='mse', optimizer='rmsprop')
+
+#net.add(Dense(320, input_dim=xTrain.shape[1], init='glorot_uniform', activation='tanh'))
+#net.add(Dropout(0.5))
+#net.add(Dense(160, init='glorot_uniform', activation='tanh'))
+#net.add(Dense(1, init='uniform', activation='linear'))
 
 net.compile(loss='mse', optimizer='rmsprop')
 
