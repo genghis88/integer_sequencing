d57e2488745d7554e929ba4698ea3601699abf1b
unknown <kprabhu@ebay.com>
2016-10-22 19:59:22 -0700

    Updated LSTM

--- src/train.py
+++ src/train.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import sys
 
+from sqlalchemy.sql.functions import concat
+
 trainingData = pd.read_csv('data/train.csv')
 
 print(trainingData)
@@ -25,7 +27,7 @@ for index, row in trainingData.iterrows():
     y[row['Id']] = nums[nums.__len__() - 1]
 
 y = pd.DataFrame.from_dict(y, orient='index')
-xTrain = pd.DataFrame.from_dict(xTrain, orient='index', dtype='uint64')
+xTrain = pd.DataFrame.from_dict(xTrain, orient='index', dtype='float')
 
 xTrain.fillna(value=0, inplace=True)
 
@@ -38,6 +40,12 @@ print('xTrain ' + str(xTrain))
 xTrain = xTrain.as_matrix()
 y = y.as_matrix()
 
+from sklearn.preprocessing import scale
+from sklearn.preprocessing import normalize
+
+xTrain = normalize(xTrain)
+xTrain = scale(xTrain)
+
 import random
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
@@ -48,19 +56,22 @@ from keras.regularizers import l2
 xTrain = xTrain.reshape(xTrain.shape + (1,))
 
 net = Sequential()
-net.add(LSTM(128, input_shape=(347, 1), return_sequences=False, go_backwards=False,
+net.add(LSTM(128, input_shape=(347, 1), return_sequences=False, go_backwards=False, consume_less='gpu',
                W_regularizer=l2(0.005), U_regularizer=l2(0.005),
                inner_init='glorot_normal', init='glorot_normal', activation='tanh'))  # try using a GRU instead, for fun
 net.add(Dense(1, activation='linear'))
 
 # try using different optimizers and different optimizer configs
-net.compile(loss='mse', optimizer='rmsprop')
+optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+net.compile(loss='mse', optimizer=optimizer)
 
 #net.add(Dense(320, input_dim=xTrain.shape[1], init='glorot_uniform', activation='tanh'))
 #net.add(Dropout(0.5))
 #net.add(Dense(160, init='glorot_uniform', activation='tanh'))
 #net.add(Dense(1, init='uniform', activation='linear'))
 
-net.fit(xTrain, y, batch_size=32, nb_epoch=5)
+net.fit(xTrain, y, batch_size=256, nb_epoch=5)
 
 y_pred = net.predict(xTrain)
+
+y_pred.to_csv('preds/pred1.csv')
